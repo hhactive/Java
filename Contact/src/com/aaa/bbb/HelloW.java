4b1f218013802de8d638a4a0eb1ba2ff057c4b11
hhactive <mattzhaofrm@gmail.com>
2021-01-18 23:23:24 -0500

    Delete Contact directory

--- Contact/src/com/aaa/bbb/HelloW.java
+++
@@ -1,8 +0,0 @@
-package com.aaa.bbb;
-
-public class HelloW {
-	public static void main(String[] args) {
-		System.out.println("1");
-	}
-
-}
--- Contact/src/module-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * 
- */
-/**
- * @author MattZ
- *
- */
-module contact {
-}
